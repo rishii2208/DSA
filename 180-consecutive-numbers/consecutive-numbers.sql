1aaebc206b9ae1411f5db0ac8df440b27fbe9e7f
Rishi <124516095+rishii2208@users.noreply.github.com>
2025-11-16 23:13:32 +0530

    Time: 513 ms (64.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+  num AS ConsecutiveNums
+FROM (
+  SELECT
+    num,
+    LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+    LEAD(num, 1) OVER (ORDER BY id) AS next_num
+  FROM
+    Logs
+) AS Subquery
+WHERE
+  num = prev_num AND num = next_num;
