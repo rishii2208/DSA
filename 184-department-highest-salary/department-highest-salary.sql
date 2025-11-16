215a66d8d40c34b5e0dcaab777ee339f5840d349
Rishi <124516095+rishii2208@users.noreply.github.com>
2025-11-16 23:15:42 +0530

    Time: 672 ms (49.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,18 @@
+SELECT
+  D.name AS Department,
+  E.name AS Employee,
+  E.salary AS Salary
+FROM
+  Employee AS E
+JOIN
+  Department AS D ON E.departmentId = D.id
+WHERE
+  (E.departmentId, E.salary) IN (
+    SELECT
+      departmentId,
+      MAX(salary)
+    FROM
+      Employee
+    GROUP BY
+      departmentId
+  );
